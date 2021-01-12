3a3553eb519a0d76c035248ef60a8f4761cfa7e8
TakeshiYoshi <ys1119.bxh@gmail.com>
2021-01-12 15:51:08 +0900

    [Fix] NG版解答例を確認しTaskシステムテストの修正を行った.

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -85,13 +85,13 @@ RSpec.describe 'Task', type: :system do
 
   describe 'Task削除' do
     let!(:task) { create(:task, project: project) }
-    
+
     context '正常系' do
       it 'Taskが削除されること' do
         visit project_tasks_path(project)
         click_link 'Destroy'
         page.driver.browser.switch_to.alert.accept
-        expect(page).not_to have_content(task.title, exact: true)
+        expect(find('.task_list')).not_to have_content task.title
         expect(Task.count).to eq 0
         expect(current_path).to eq project_tasks_path(project)
       end
