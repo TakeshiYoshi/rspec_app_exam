5fcfda6db454e45ec1deec067eca7512faedf762
TakeshiYoshi <ys1119.bxh@gmail.com>
2021-01-12 15:43:56 +0900

    [Fix] Taskシステムテストのリファクタリングを行った.

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Task', type: :system do
-  describe 'Task一覧' do
-    let!(:project) { create(:project) }
-    let!(:task) { create(:task, project: project) }
+  let(:project) { create(:project) }
+  let(:task) { create(:task, project: project) }
 
+  describe 'Task一覧' do
     context '正常系' do
       it '一覧ページにアクセスした場合、Taskが表示されること' do
-        # TODO: ローカル変数ではなく let を使用してください
         visit project_tasks_path(project)
         expect(page).to have_content task.title
         expect(Task.count).to eq 1
@@ -15,7 +14,6 @@ RSpec.describe 'Task', type: :system do
       end
 
       it 'Project詳細からTask一覧ページにアクセスした場合、Taskが表示されること' do
-        # FIXME: テストが失敗するので修正してください
         visit project_path(project)
         click_link 'View Todos'
         switch_to_window(windows.last)
@@ -27,11 +25,8 @@ RSpec.describe 'Task', type: :system do
   end
 
   describe 'Task新規作成' do
-    let(:project) { create(:project) }
-
     context '正常系' do
       it 'Taskが新規作成されること' do
-        # TODO: ローカル変数ではなく let を使用してください
         visit project_tasks_path(project)
         click_link 'New Task'
         fill_in 'Title', with: 'test'
@@ -44,12 +39,8 @@ RSpec.describe 'Task', type: :system do
   end
 
   describe 'Task詳細' do
-    let(:project) { create(:project) }
-    let(:task) { create(:task, project: project) }
-    
     context '正常系' do
       it 'Taskが表示されること' do
-        # TODO: ローカル変数ではなく let を使用してください
         visit project_task_path(project, task)
         expect(page).to have_content(task.title)
         expect(page).to have_content(task.status)
@@ -60,13 +51,10 @@ RSpec.describe 'Task', type: :system do
   end
 
   describe 'Task編集' do
-    let(:project) { create(:project) }
-    let(:task) { create(:task, project: project) }
     let(:done_task) { create(:task, :done, project: project) }
 
     context '正常系' do
       it 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
-        # FIXME: テストが失敗するので修正してください
         visit edit_project_task_path(project, task)
         fill_in 'Deadline', with: Time.current
         click_button 'Update Task'
@@ -76,7 +64,6 @@ RSpec.describe 'Task', type: :system do
       end
 
       it 'ステータスを完了にした場合、Taskの完了日に今日の日付が登録されること' do
-        # TODO: ローカル変数ではなく let を使用してください
         visit edit_project_task_path(project, task)
         select 'done', from: 'Status'
         click_button 'Update Task'
@@ -86,7 +73,6 @@ RSpec.describe 'Task', type: :system do
       end
 
       it '既にステータスが完了のタスクのステータスを変更した場合、Taskの完了日が更新されないこと' do
-        # TODO: FactoryBotのtraitを利用してください
         visit edit_project_task_path(project, done_task)
         select 'todo', from: 'Status'
         click_button 'Update Task'
@@ -98,11 +84,9 @@ RSpec.describe 'Task', type: :system do
   end
 
   describe 'Task削除' do
-    let!(:project) { create(:project) }
     let!(:task) { create(:task, project: project) }
-
+    
     context '正常系' do
-      # FIXME: テストが失敗するので修正してください
       it 'Taskが削除されること' do
         visit project_tasks_path(project)
         click_link 'Destroy'
